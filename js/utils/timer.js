6a9a56be0a4ea80e6a9140d16d4626788c607859
Simon Penel <simon.penel@univ-lyon1.fr>
2021-09-29 15:09:49 +0200

    Add the inactivity detection

---
+++ js/utils/timer.js
@@ -0,0 +1,32 @@
+var timeoutInMiliseconds = 20000;
+var timeoutId; 
+ 
+function startTimer() { 
+    // window.setTimeout returns an Id that can be used to start and stop a timer
+    timeoutId = window.setTimeout(doInactive, timeoutInMiliseconds)
+}
+ 
+function doInactive() {
+    // does whatever you need it to actually do - probably signs them out or stops polling the server for info
+	console.log("Session inactive, back to root");
+	zoomTo(1);
+	map.removeLayer(SPfocus);    
+	if (selected) {
+		degrise(selected);
+	}
+
+}
+
+function resetTimer() { 
+    window.clearTimeout(timeoutId)
+    startTimer();
+}
+function setupTimers () {
+    document.addEventListener("mousemove", resetTimer, false);
+    document.addEventListener("mousedown", resetTimer, false);
+    document.addEventListener("keypress", resetTimer, false);
+    document.addEventListener("touchmove", resetTimer, false);
+     
+    startTimer();
+}
+ 
